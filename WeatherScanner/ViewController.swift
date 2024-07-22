44ad808a8b838449c07d2135767f8a3799fea233
yujin <ydbwlsjki60@gmail.com>
2024-07-22 21:36:19 +0900

    Initial Commit

---
+++ WeatherScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherScanner
+//
+//  Created by 조유진 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

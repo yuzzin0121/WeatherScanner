ce5c234e21342fe8f669cd2b5105f53649ce1503
yujin <ydbwlsjki60@gmail.com>
2024-07-24 08:50:48 +0900

    [Feat] 셀에 사용될 모델 구현

---
+++ WeatherScanner/Sources/Model/Other/CurrentWeather.swift
@@ -0,0 +1,15 @@
+//
+//  CurrentWeather.swift
+//  WeatherScanner
+//
+//  Created by 조유진 on 7/24/24.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temp: Int
+    let weather: String
+    let tempMin: Int
+    let tempMax: Int
+}

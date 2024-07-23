b68455e14d941f5cb274d68a18b82bab569df858
yujin <ydbwlsjki60@gmail.com>
2024-07-23 20:11:18 +0900

    [Build] WeatherViewModel 구성

---
+++ WeatherScanner/Sources/Scene/Weather/ViewModel/WeatherViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherViewModel.swift
+//  WeatherScanner
+//
+//  Created by 조유진 on 7/23/24.
+//
+
+import RxSwift
+import RxCocoa
+
+final class WeatherViewModel: ViewModelType {
+    var disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+}

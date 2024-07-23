76e6c2e499dec797071060125b18b50446b0e7e7
yujin <ydbwlsjki60@gmail.com>
2024-07-23 22:27:45 +0900

    [Feat] json data 구조체 배열로 Decode

--- WeatherScanner/Sources/Util/Manager/CityManager.swift
+++ WeatherScanner/Sources/Util/Manager/CityManager.swift
@@ -10,6 +10,25 @@ import Foundation
 final class CityManager {
     static let shared = CityManager()
     
+    // 구조체 배열로 변환
+    func getCityList() -> [City] {
+        print(#function)
+        guard let url = getJsonUrl() else {
+            // 에러 처리 필요
+            return []
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let cityList = try decoder.decode([City].self, from: data)
+            return cityList
+        } catch {
+            print("Failed to load and decode JSON: \(error)")
+            return []
+        }
+    }
+    
     // json url 반환
     private func getJsonUrl() -> URL? {
         let fileName = "reduced_citylist"

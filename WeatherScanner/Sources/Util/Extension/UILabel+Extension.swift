79b7bc59f09b82c79b28cb3d1e1133232bb8c03e
yujin <ydbwlsjki60@gmail.com>
2024-07-24 09:04:55 +0900

    [Feat] extension UILabel - design 기능 구현
    공통된 속성 설정을 캡슐화

---
+++ WeatherScanner/Sources/Util/Extension/UILabel+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UILabel+Extension.swift
+//  WeatherScanner
+//
+//  Created by 조유진 on 7/24/24.
+//
+
+import Foundation
+
+extension UILabel {
+    func design(text: String = "",
+                textColor: UIColor = Color.white,
+                font: UIFont = .systemFont(ofSize: 14, weight: .regular),
+                textAlignment: NSTextAlignment = .center,
+                numberOfLines: Int = 1) {
+        self.text = text
+        self.textAlignment = textAlignment
+        self.font = font
+        self.textColor = textColor
+        self.numberOfLines = numberOfLines
+    }
+    
+}
